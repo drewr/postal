84285cf9118882b49e6413cdbb00409abc3ad5cc
Drew Raines <aaraines@gmail.com>
2009-04-20 12:55:39 -0500

    Split sending options, depending on :host.  Add sendmail support.

--- src/clj/com/draines/postal/core.clj
+++ src/clj/com/draines/postal/core.clj
@@ -3,99 +3,76 @@
         [com.draines.postal.date :only [make-date]])
   (:import [java.util Date Properties]
            [javax.mail Session Transport Message$RecipientType]
-           [javax.mail.internet MimeMessage InternetAddress]
-           [com.sun.mail.smtp SMTPSSLTransport]))
+           [javax.mail.internet MimeMessage InternetAddress]))
 
-;;  Properties props = new Properties();
-;;  props.put("mail.smtp.host", "my-mail-server");
-;;  props.put("mail.from", "me@example.com");
-;;  Session session = Session.getInstance(props, null);
-;;
-;;  try {
-;;      MimeMessage msg = new MimeMessage(session);
-;;      msg.setFrom();
-;;      msg.setRecipients(Message.RecipientType.TO,
-;;                        "you@example.com");
-;;      msg.setSubject("JavaMail hello world example");
-;;      msg.setSentDate(new Date());
-;;      msg.setText("Hello, world!\n");
-;;      Transport.send(msg);
-;;  } catch (MessagingException mex) {
-;;      System.out.println("send failed, exception: " + mex);
-;;  }
+(defn add-recipient [jmsg rtype addr]
+  (doto jmsg (.addRecipient rtype (InternetAddress. addr))))
 
-;; Session.getTransport() and Transport.connect()
-
-(defn get-message [msg]
-  (get msg :MimeMessage msg))
-
-(defn get-recipients [msg]
-  (map (comp str
-             #(.getAddress %))
-       (.getAllRecipients (get-message msg))))
-
-(defn add-recipient [msg rtype addr]
-  (doto (get-message msg) (.addRecipient rtype (InternetAddress. addr))))
-
-(defn add-recipients [msg rtype addrs]
+(defn add-recipients [jmsg rtype addrs]
   (if (string? addrs)
-    (add-recipient msg rtype addrs)
+    (add-recipient jmsg rtype addrs)
     (doseq [addr addrs]
-      (add-recipient msg rtype addr)))
-  msg)
+      (add-recipient jmsg rtype addr)))
+  jmsg)
 
-(defn set-from [msg from]
-  (.setFrom (get-message msg) (InternetAddress. from)))
-
-(defn set-subject [msg subj]
-  (.setSubject (get-message msg) subj))
-
-(defn set-body [msg body]
-  (.setText (get-message msg) body))
-
-(defn set-date [msg date]
-  (.setSentDate (get-message msg) date))
-
-(defn get-from [msg]
-  (.getFrom (get-message msg)))
-
-(defn get-subject [msg]
-  (.getSubject (get-message msg)))
-
-(defn get-body [msg]
-  (.getContent (get-message msg)))
-
-(defn get-date [msg]
-  (.getSentDate (get-message msg)))
-
-(defn make-message [attrs]
-  (let [{:keys [from to cc bcc date subject body host]} attrs
+(defn make-jmessage [msg]
+  (let [{:keys [from to cc bcc date subject body host]} msg
         props (doto (java.util.Properties.)
-                (.put "mail.smtp.host" host)
+                (.put "mail.smtp.host" (or host "not.provided"))
                 (.put "mail.from" from))
-        msg (merge attrs {:Session (Session/getInstance props)})
-        msg (merge msg {:MimeMessage (MimeMessage. (:Session msg))})]
-    (add-recipients msg Message$RecipientType/TO to)
-    (add-recipients msg Message$RecipientType/CC cc)
-    (add-recipients msg Message$RecipientType/BCC bcc)
-    (set-from msg from)
-    (set-subject msg subject)
-    (set-body msg body)
-    (set-date msg date)
-    msg))
+        session (Session/getInstance props)
+        jmsg (MimeMessage. (:Session msg))]
+    (add-recipients jmsg Message$RecipientType/TO to)
+    (add-recipients jmsg Message$RecipientType/CC cc)
+    (add-recipients jmsg Message$RecipientType/BCC bcc)
+    (.setFrom jmsg (InternetAddress. from))
+    (.setSubject jmsg subject)
+    (.setText jmsg body)
+    (.setSentDate jmsg (or date (make-date)))
+    jmsg))
 
-(deftest all
-  (let [date (make-date "yyyy-MM-dd" "2009-01-01")
-        msg (make-message {:host "smtp.comcast.net"
-                           :from "fee@bar.dom"
-                           :to "Foo Bar <foo@bar.dom>"
-                           :cc ["baz@bar.dom" "quux@bar.dom"]
-                           :date date
-                           :subject "Test"
-                           :body "Test!"})]
-    (is (= '("foo@bar.dom" "baz@bar.dom" "quux@bar.dom") (get-recipients msg)))
-    (is (= "Test" (get-subject msg)))
-    (is (= "Test!" (get-body msg)))
-    (is (= date (get-date msg)))))
+(defn recipients [msg]
+  (let [jmsg (make-jmessage msg)]
+    (map str (.getAllRecipients jmsg))))
 
-(run-tests)
+(defn sender [msg]
+  (or (:sender msg) (:from msg)))
+
+(defn message->str [msg]
+  (let [out (java.io.ByteArrayOutputStream.)]
+    (.writeTo (make-jmessage msg) out)
+    (str out)))
+
+(defn smtp-send [msg]
+  (let [jmsg (make-jmessage msg)]
+    (Transport/send jmsg)))
+
+(defn sendmail-send [msg]
+  (let [mail (message->str msg)
+        cmd (concat
+             ["/usr/sbin/sendmail" (format "-f %s" (sender msg))]
+             (recipients msg))
+        pb (ProcessBuilder. cmd)
+        p (.start pb)
+        smtp (java.io.PrintStream. (.getOutputStream p))]
+    (.print smtp mail)
+    (.close smtp)
+    (.waitFor p)
+    (condp = (.exitValue p)
+      0 :success
+      [:error (.exitValue p)])))
+
+(defn send-message [msg]
+  (if (:host msg)
+    (smtp-send msg)
+    (sendmail-send msg)))
+
+(comment
+  (send-message {:from "fee@bar.dom"
+                 :to "Foo Bar <foo@bar.dom>"
+                 :cc ["baz@bar.dom" "quux@bar.dom"]
+                 :date (make-date "yyyy-MM-dd" "2009-01-01")
+                 :subject "Test"
+                 :body "Test!"})
+
+)
