d5b978bc16abf5e9386ff60a3654148ddb173abd
Drew Raines <aaraines@gmail.com>
2009-04-20 16:45:19 -0500

    Check for sane message.

--- src/clj/com/draines/postal/core.clj
+++ src/clj/com/draines/postal/core.clj
@@ -3,6 +3,11 @@
         [com.draines.postal.smtp :only [smtp-send]]))
 
 (defn send-message [msg]
+  (when-not (and (:from msg)
+                 (:to msg)
+                 (:subject msg)
+                 (:body msg))
+    (throw (Exception. "message needs at least :from, :to, :subject, and :body")))
   (if (:host msg)
     (smtp-send msg)
     (sendmail-send msg)))
