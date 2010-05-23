129289d3fd85145e4c1a746f9445c5bdfcf95a5a
Drew Raines <aaraines@gmail.com>
2010-05-22 21:45:56 -0500

    Add project.clj.

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject postal "1.2.0-SNAPSHOT"
+  :resources-path "etc"
+  :repositories {"java.net" "http://download.java.net/maven/2"}
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [javax.mail/mail "1.4.4-SNAPSHOT"
+                  :exclusions [javax.activation/activation]]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :source-path "src/clj"
+  :namespaces [com.draines.postal.core
+               com.draines.postal.message
+               com.draines.postal.date
+               com.draines.postal.main
+               com.draines.postal.sendmail
+               com.draines.postal.smtp])
