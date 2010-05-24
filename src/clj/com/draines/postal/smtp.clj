a52d5817b78db988fdc5065b503a2d26a8691236
Drew Raines <aaraines@gmail.com>
2010-05-23 22:33:56 -0500

    Extend smtp-send to inject multiple messages through single connection.

--- src/clj/com/draines/postal/smtp.clj
+++ src/clj/com/draines/postal/smtp.clj
@@ -1,12 +1,25 @@
 (ns com.draines.postal.smtp
   (:use [com.draines.postal.message :only [make-jmessage]])
-  (:import [javax.mail Transport]))
+  (:import [javax.mail Transport Session]))
 
-(defn smtp-send [msg]
-  (let [jmsg (make-jmessage msg)]
-    (try
-     (Transport/send jmsg)
-     {:code 0 :error :SUCCESS :message "message sent"}
-     (catch Exception e
-       {:code 99 :error (class e) :message (.getMessage e)}))))
+(defn smtp-send
+  ([msg]
+     (let [jmsg (make-jmessage msg)]
+       (try
+         (Transport/send jmsg)
+         {:code 0 :error :SUCCESS :message "message sent"}
+         (catch Exception e
+           {:code 99 :error (class e) :message (.getMessage e)}))))
+  ([host port & msgs]
+     (let [session (doto (Session/getInstance
+                          (doto (java.util.Properties.)
+                            (.put "mail.smtp.host" (or host "localhost"))
+                            (.put "mail.smtp.port" (or port "25"))))
+                     (.setDebug false))]
+       (with-open [transport (doto (-> session (.getTransport "smtp"))
+                               (.connect "" ""))]
+         (let [jmsgs (map #(make-jmessage % session) msgs)]
+           (doseq [jmsg jmsgs]
+             (.sendMessage transport jmsg (.getAllRecipients jmsg))))))))
+
 
