5641673bd2de263c1fe58598558497ed96586324
Drew Raines <aaraines@gmail.com>
2010-05-25 09:20:19 -0500

    Add c.d.postal.stress for load-testing.

---
+++ src/clj/com/draines/postal/stress.clj
@@ -0,0 +1,76 @@
+(ns com.draines.postal.stress
+  (:import [java.util Date]
+           [java.text SimpleDateFormat]
+           [java.util.concurrent CountDownLatch])
+  (:use [clojure.test]
+        [com.draines.postal.smtp :only [smtp-send]]
+        [com.draines.postal.message :only [make-fixture]]))
+
+(def DATEFORMAT (SimpleDateFormat. "yyyy-MM-dd.HH:mm:ss"))
+
+(def *from* "foo@lolz.dom")
+(def *to* "bar@lolz.dom")
+
+(defonce *counter* (atom 0))
+
+(defn reset-counter! []
+  (reset! *counter* 0))
+
+(def *logger* (agent nil))
+
+(defn log* [x s]
+  (let [s* (format "%s %s"
+                   (.format DATEFORMAT (Date.))
+                   (apply str (interpose " " s)))]
+    (println s*)
+    (flush)
+    s*))
+
+(defn log [& s]
+  (send-off *logger* log* s))
+
+(defn partition-work
+  "Break up total number of messages evenly over desired number of threads."
+  [n ts]
+  (if (> ts n)
+    [n]
+    (let [m (int (Math/floor (float (/ n ts))))]
+      (loop [acc [] n n]
+        (if (>= n m)
+          (recur (conj acc m) (- n m))
+          (if (pos? n)
+            (conj acc n)
+            acc))))))
+
+(defn spam
+  ([host port from to n]
+     (spam host from to port n 0))
+  ([host port from to n delay]
+     (log (format "(thread: %s) %s msgs -> %s"
+                  (-> (Thread/currentThread) .getId)
+                  n host))
+     (let [date (.format DATEFORMAT (Date.))]
+       (dotimes [x n]
+         (smtp-send host port (make-fixture from to))
+         (swap! *counter* inc)
+         (Thread/sleep delay))
+       n))
+  ([host port from to n delay threads]
+     (let [latch (CountDownLatch. threads)
+           res (doall
+                (map #(future
+                       (let [ct (spam host port from to % delay)]
+                         (.countDown latch)
+                         ct))
+                     (partition-work n threads)))]
+       (.await latch)
+       (reduce #(+ %1 (deref %2)) 0 res))))
+
+
+(deftest test-partition-work
+  (is (= [5 5] (partition-work 10 2)))
+  (is (= [3 3 3 1] (partition-work 10 3)))
+  (is (= [111 111 111 111 111 111 111 111 111 111 3]
+           (partition-work 1113 10))))
+
+
