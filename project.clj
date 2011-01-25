b172f7880e11c6d1ec53ca22fa4669fc4468bd30
Paul Stadig <paul@stadig.name>
2011-01-25 13:49:20 -0500

    Remove snapshots for 1.2.0 release of clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.draines/postal "1.4.0-SNAPSHOT"
   :resources-path "etc"
   :repositories {"java.net" "http://download.java.net/maven/2"}
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [javax.mail/mail "1.4.4-SNAPSHOT"
                   :exclusions [javax.activation/activation]]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
