5a5085abc71b840916599e164a5f6b7b1821ab8d
Drew Raines <aaraines@gmail.com>
2011-02-01 11:34:11 -0600

    Add lein-clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject com.draines/postal "1.4.0-SNAPSHOT"
   :resources-path "etc"
-  :repositories {"java.net" "http://download.java.net/maven/2"}
+  :repositories {"java.net" "http://download.java.net/maven/2"
+                 "clojars" "http://clojars.org/repo"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [javax.mail/mail "1.4.4"
                   :exclusions [javax.activation/activation]]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.6.0"]]
   :source-path "src/clj"
   :main com.draines.postal.main)
