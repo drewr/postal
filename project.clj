6d314127c8ab9b719fee1abfb1919487392d873f
Drew Raines <aaraines@gmail.com>
2011-08-11 22:03:45 -0500

    clojure-1.3.

--- project.clj
+++ project.clj
@@ -2,9 +2,8 @@
   :resources-path "etc"
   :repositories {"java.net" "http://download.java.net/maven/2"
                  "clojars" "http://clojars.org/repo"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [javax.mail/mail "1.4.4"
                   :exclusions [javax.activation/activation]]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+  :dev-dependencies [[swank-clojure "1.3.1"]
                      [lein-clojars "0.6.0"]])
