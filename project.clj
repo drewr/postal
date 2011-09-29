bfa2bf925a2efc737c5be36401ab473f66448593
Phil Hagelberg <technomancy@gmail.com>
2011-09-29 12:31:05 -0700

    Switch to Clojure 1.3.

--- project.clj
+++ project.clj
@@ -1,10 +1,5 @@
 (defproject com.draines/postal "1.6-SNAPSHOT"
-  :resources-path "etc"
-  :repositories {"java.net" "http://download.java.net/maven/2"
-                 "clojars" "http://clojars.org/repo"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :repositories {"java.net" "http://download.java.net/maven/2"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [javax.mail/mail "1.4.4"
-                  :exclusions [javax.activation/activation]]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.6.0"]])
+                  :exclusions [javax.activation/activation]]])
