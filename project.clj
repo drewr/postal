ccf77f5d35e8c5dc8f6e3dadcf110b394add0ca2
Drew Raines <drew@raines.me>
2012-04-17 15:05:42 -0500

    Remove repl-utils.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :repositories {"java.net" "http://download.java.net/maven/2"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [javax.mail/mail "1.4.4"
-                  :exclusions [javax.activation/activation]]
-                 [org.clojure.contrib/repl-utils "1.3.0-alpha4"]])
+                  :exclusions [javax.activation/activation]]])
