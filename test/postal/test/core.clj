bef07acfb705f08cb36d899b16c0d29c1cdf72e0
Drew Raines <drew@raines.me>
2012-04-17 16:25:25 -0500

    Add send-message integration test. [#22]

--- test/postal/test/core.clj
+++ test/postal/test/core.clj
@@ -1,5 +1,23 @@
 (ns postal.test.core
-  (:use [postal.core]
-        [postal.sendmail :only [sendmail-send]]
-        [postal.smtp :only [smtp-send]]
-        [postal.stress :only [spam]]))
+  (:require [postal.core :as postal]
+            [postal.sendmail :as local]
+            [postal.smtp :as smtp])
+  (:use [clojure.test]))
+
+(defn sendmail-send [msg]
+  (merge {:which :sendmail} msg (meta msg)))
+
+(defn smtp-send [server msg]
+  (merge {:which :smtp} server msg (meta msg)))
+
+(deftest integration
+  (with-redefs [local/sendmail-send sendmail-send
+                smtp/smtp-send smtp-send]
+    (is (= :smtp (:which
+                  (postal/send-message ^{:host "localhost"
+                                         :port "25"}
+                                       {:from "foo@example.com"
+                                        :to "bar@example.com"}))))
+    (is (= :sendmail (:which
+                      (postal/send-message {:from "foo@example.com"
+                                            :to "bar@example.com"}))))))
