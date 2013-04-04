24fbc74409fc965cbf97d4b26a6aa67570d1ef95
Drew Raines <drew@raines.me>
2013-04-04 16:09:04 -0500

    Move clojure out of proper deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject com.draines/postal "1.10.0"
   :repositories {"java.net" "http://download.java.net/maven/2"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [commons-codec "1.7"]
+  :dependencies [[commons-codec "1.7"]
                  [javax.mail/mail "1.4.4"
-                  :exclusions [javax.activation/activation]]])
+                  :exclusions [javax.activation/activation]]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
