0a690fc600b9889c0a1c1e603efede31deb04cad
Andrew Raines <andrew@raines.me>
2013-08-04 15:43:56 -0500

    Release 1.11.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.draines/postal "1.11-SNAPSHOT"
+(defproject com.draines/postal "1.11.0"
   :repositories {"java.net" "http://download.java.net/maven/2"}
   :dependencies [[commons-codec "1.7"]
                  [javax.mail/mail "1.4.4"
