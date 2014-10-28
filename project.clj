65a79f8dfa2ee365391646aac905d668518c5d3e
Andrew Raines <andrew@raines.me>
2014-10-28 06:30:59 -0700

    Add metadata to appease clojars
    Conflicts:
    	project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject com.draines/postal "1.12-SNAPSHOT"
+  :description "JavaMail on Clojure"
+  :url "https://github.com/drewr/postal"
+  :license {:name "MIT"
+            :url "http://en.wikipedia.org/wiki/MIT_License"
+            :distribution :repo
+            :comments "Use at your own risk"}
   :repositories {"java.net" "http://download.java.net/maven/2"}
   :dependencies [[commons-codec "1.9"]
                  [javax.mail/mail "1.4.4"
