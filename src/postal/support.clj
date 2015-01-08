db6ac87b4e855174bdb02b1e979796191ef7b16e
Jesse Bouwman <jesse@threatgrid.com>
2015-01-08 14:52:15 -0600

    Allow local address configuration for multihomed hosts
    :localhost sets the host name used in the EHLO command.
    
    :localaddress sets the address to bind to when generating the outbound
    connection (for multihomed hosts).

--- src/postal/support.clj
+++ src/postal/support.clj
@@ -31,14 +31,16 @@
   `(when ~condition
      (doto ~arg ~@body)))
 
-(defn make-props [sender {:keys [host port user tls]}]
+(defn make-props [sender {:keys [host port user tls localhost localaddress]}]
   (doto (Properties.)
     (.put "mail.smtp.host" (or host "not.provided"))
     (.put "mail.smtp.port" (or port "25"))
     (.put "mail.smtp.auth" (if user "true" "false"))
     (do-when sender (.put "mail.smtp.from" sender))
     (do-when user (.put "mail.smtp.user" user))
-    (do-when tls  (.put "mail.smtp.starttls.enable" "true"))))
+    (do-when tls  (.put "mail.smtp.starttls.enable" "true"))
+    (do-when localhost (.put "mail.smtp.localhost" localhost))
+    (do-when localaddress (.put "mail.smtp.localaddress" localaddress))))
 
 (defn hostname []
   (.getHostName (java.net.InetAddress/getLocalHost)))
