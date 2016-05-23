253831e1500fdd4cec879a15070d69035b3aeca4
Ivan Kryvoruchko <gildraug@gmail.com>
2016-05-23 13:01:06 +0300

    Bump Java Mail version to 1.5.5

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
             :comments "Use at your own risk"}
   :repositories {"java.net" "http://download.java.net/maven/2"}
   :dependencies [[commons-codec "1.9"]
-                 [javax.mail/mail "1.4.4"
+                 [com.sun.mail/javax.mail "1.5.5"
                   :exclusions [javax.activation/activation]]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
