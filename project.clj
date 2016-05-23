f16fa8074a0989b48b5f0ca3bd2cbb283dcf2083
Drew Raines <drew@raines.me>
2016-05-23 23:28:38 +0100

    javamail 1.5.5

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
             :comments "Use at your own risk"}
   :repositories {"java.net" "http://download.java.net/maven/2"}
   :dependencies [[commons-codec "1.9"]
-                 [javax.mail/mail "1.4.4"
-                  :exclusions [javax.activation/activation]]]
+                 [com.sun.mail/javax.mail "1.5.5"]
+                 [javax.mail/javax.mail-api "1.5.5"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
