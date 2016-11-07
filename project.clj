1920ff429fdfcdf10c6c9c53448af1bb52713a13
Drew Raines <drew@raines.me>
2016-11-07 10:27:30 -0600

    Version 2.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.draines/postal "2.0.2"
+(defproject com.draines/postal "2.0.3-SNAPSHOT"
   :description "JavaMail on Clojure"
   :url "https://github.com/drewr/postal"
   :license {:name "MIT"
